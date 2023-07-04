68044a096768c12599567a91223689cf4aea2e35
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2023-07-04 17:48:15 +0200

    simple calculator finished

---
+++ main.py
@@ -0,0 +1,45 @@
+theMessage =''' 
+    Hello my friend how are you I´m going to give a list to do simple operations
+    1. sum
+    2. divide
+    3. substraction
+    4. multiplication
+
+
+    only tell what do you like and after put the numbers 
+
+'''
+
+print(theMessage)
+
+Operation = input("put the operation right here: ")
+
+print("now tell me the numbers")
+Number1 = int(input("first Number: "))
+Number2 = int(input("second Number: "))
+
+def sum(number1 ,number2 ,operation): 
+    if operation == "1":
+        result = number1 + number2
+        print(result)
+    
+def divide(number1,number2,operation):
+    if operation == "2":
+            result =   number1 / number2
+            print(result)
+
+def substraction(number1,number2,operation):
+     if operation == "3":
+          result = number1 - number2
+          print(result)
+        
+def multiplicattion(number1,number2,operation):
+     if operation == "4":
+          result = number1 * number2
+          print(result)
+
+
+multiplicattion(Number1,Number2,Operation)
+substraction(Number1,Number2,Operation)
+sum(Number1,Number2,Operation)
+divide(Number1,Number2,Operation)
